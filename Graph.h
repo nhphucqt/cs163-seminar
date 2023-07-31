65832a04ad175e64610813afb27a9ea28b899557
nhphucqt <hoangphuc.qti@gmail.com>
2023-07-31 15:17:04 +0700

    Upload via files

---
+++ Graph.h
@@ -0,0 +1,25 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+
+class GraphEdge {
+public:
+    int u, v, w;
+
+    GraphEdge() {
+
+    }
+
+    GraphEdge()
+};
+
+// Graph Class
+// has numNode nodes indexed from 1 to numNode
+// has a vector of edges (GraphEdge)
+class Graph {
+private:
+    int numNode;
+    std::vector<GraphEdge>
+};
+
+#endif
